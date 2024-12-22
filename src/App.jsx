f9c2e823d705ded91e0ec8c157b68ee4205061a0
Hazem Mohamed Elmaghraby <hazemelmaghraby.work@gmail.com>
2024-12-22 15:38:42 +0200

    react-router-update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Register from './components/Registeration/Register.jsx';
 import ShowData from './components/Registeration/showData.jsx';
 import Landing from './components/Home/Landing.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from 'react-router-dom';
 import Profile from './components/Registeration/profile/Profile.jsx';
 import PremiumPlans from './components/PremuimPlans/PremuimPlans.jsx';
 import Marketplace from './components/Marketplace/Marketplace.jsx';
@@ -25,37 +26,87 @@ import AddProductTest from './components/Admin/AddProduct/AddProductTest.jsx';
 // import { SpeedInsights } from "@vercel/speed-insights/next"
 // import Test from './components/Home/test.jsx';
 
+
+
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+);
+
 const App = () => {
+
+  const routerr = createBrowserRouter(createRoutesFromElements(
+    <Route>
+
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Landing />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/data" element={<ShowData />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/premium" element={<PremiumPlans />} />
+        <Route path="/market" element={<Marketplace />} />
+        <Route path="/marketManage" element={<MarketpalceManagement />} />
+        <Route path="/productAdd" element={<AddProduct />} />
+        <Route path="/productRemove" element={<MarketpalceManagement />} />
+        <Route path="/social" element={<SocialMedia />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pro" element={<ProductsList />} />
+        <Route path="/testt" element={<TestFirestore />} />
+        <Route path="/ourteam" element={<OurTeam />} />
+
+        <Route path='/cart' element={<Cart />}></Route>
+      </Route>
+
+    </Route>
+  ))
+
   return (
     <div>
-      {/* Navbar is placed outside Routes to appear on all pages */}
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Landing />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/data" element={<ShowData />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/premium" element={<PremiumPlans />} />
-          <Route path="/market" element={<Marketplace />} />
-          <Route path="/marketManage" element={<MarketpalceManagement />} />
-          <Route path="/productAdd" element={<AddProduct />} />
-          <Route path="/productRemove" element={<MarketpalceManagement />} />
-          <Route path="/social" element={<SocialMedia />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/pro" element={<ProductsList />} />
-          <Route path="/testt" element={<TestFirestore />} />
-          <Route path="/ourteam" element={<OurTeam />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/testing" element={<AddProductTest />} />
-          {/* <Route path="/ourTeam" element={<OurTeam />} /> */}
-          {/* <Route path="/test" element={<Test />} /> */}
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+
+      <RouterProvider router={routerr} />
     </div>
-  );
-};
+  )
+}
 
 export default App;
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* Navbar is placed outside Routes to appear on all pages */}
+//       <BrowserRouter>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/" element={<Landing />} />
+//           <Route path="/register" element={<Register />} />
+//           <Route path="/data" element={<ShowData />} />
+//           <Route path="/profile" element={<Profile />} />
+//           <Route path="/premium" element={<PremiumPlans />} />
+//           <Route path="/market" element={<Marketplace />} />
+//           <Route path="/marketManage" element={<MarketpalceManagement />} />
+//           <Route path="/productAdd" element={<AddProduct />} />
+//           <Route path="/productRemove" element={<MarketpalceManagement />} />
+//           <Route path="/social" element={<SocialMedia />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="/pro" element={<ProductsList />} />
+//           <Route path="/testt" element={<TestFirestore />} />
+//           <Route path="/ourteam" element={<OurTeam />} />
+//           <Route path="/cart" element={<Cart />} />
+//           <Route path="/testing" element={<AddProductTest />} />
+//           {/* <Route path="/ourTeam" element={<OurTeam />} /> */}
+//           {/* <Route path="/test" element={<Test />} /> */}
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { Provider } from "react-redux";
 import store from "./app/store.js";
 import './index.css';
 import App from './App.jsx';
+import ErrorBoundary from './constants/components/ErrorBoundery.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </Provider>
-)
+);
